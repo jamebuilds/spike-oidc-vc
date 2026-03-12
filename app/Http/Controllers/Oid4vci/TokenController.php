8cec0f052508d335b0c3b7e084a76ea758b46ef2
Jame Ng <jamebuilds.com>
2026-03-12 16:21:59 +0800

    WIP

--- app/Http/Controllers/Oid4vci/TokenController.php
+++ app/Http/Controllers/Oid4vci/TokenController.php
@@ -20,6 +20,8 @@ class TokenController extends Controller
         $grantType = $request->input('grant_type');
 
         if ($grantType !== 'urn:ietf:params:oauth:grant-type:pre-authorized_code') {
+            Log::warning('OID4VCI Token: unsupported grant type', ['grant_type' => $grantType]);
+
             return response()->json([
                 'error' => 'unsupported_grant_type',
             ], 400);
@@ -28,6 +30,8 @@ class TokenController extends Controller
         $code = $request->input('pre-authorized_code');
 
         if (empty($code)) {
+            Log::warning('OID4VCI Token: missing pre-authorized_code');
+
             return response()->json([
                 'error' => 'invalid_request',
                 'error_description' => 'Missing pre-authorized_code',
@@ -37,11 +41,15 @@ class TokenController extends Controller
         $result = $session->exchangeCode($code);
 
         if ($result === null) {
+            Log::warning('OID4VCI Token: invalid or expired code', ['code_prefix' => substr($code, 0, 8).'...']);
+
             return response()->json([
                 'error' => 'invalid_grant',
                 'error_description' => 'Invalid or expired pre-authorized code',
             ], 400);
         }
+
+        Log::info('OID4VCI Token: success', ['offer_id' => $result['offer_id']]);
 
         return response()->json([
             'access_token' => $result['access_token'],
